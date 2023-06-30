8f6b8b0c46eddbaf7f4e5669724df87a1e48650f
Ghulamsss <hafidghulams@gmail.com>
2023-06-30 11:50:53 -0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello Belek!
+      Dr Laser!
     </h1>
   )
 }
